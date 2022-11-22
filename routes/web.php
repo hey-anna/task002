69e476702785097ba5228722f3b34017175eab46
yaeyoung.kang <yaeyoung.kang@lgepartner.com>
2022-11-22 23:20:31 +0900

    221122_002

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function() {
+    return view('hello');
+});
+
+// Route::get('/aaa', function () {
+//     return view('welcome');
+// });
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
